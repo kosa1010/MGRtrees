c0723c6561c94ee46cb38af8d0e9d1f0581d1c1d
kosa1010 <kosa1010@o2.pl>
2016-05-28 20:18:35 +0200

    Metoda tymczasowo testująca

--- MGRTrees/src/main/java/com/mycompany/mgrtrees/MainController.java
+++ MGRTrees/src/main/java/com/mycompany/mgrtrees/MainController.java
@@ -6,7 +6,6 @@
 package com.mycompany.mgrtrees;
 
 import java.util.ArrayList;
-import loadData.DataObject;
 
 /**
  *
@@ -14,51 +13,42 @@ import loadData.DataObject;
  */
 public class MainController {
 
+    static ArrayList<Double> entropies = new ArrayList<>();
+    static String[] heather = {"temperatura", "ciśnienie", "wysokość", "samopoczucie"};
+    static int[][] data = {
+        {1, 1, 0, 1},
+        {1, 1, 1, 1},
+        {1, 0, 0, 1},
+        {0, 0, 1, 1},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 2, 1, 0},
+        {0, 2, 0, 0},
+        {1, 2, 0, 0},
+        {1, 2, 0, 0}};
+    static double entropy;
+    static int[] a = {6, 4};
+    static int[] b = {2, 3, 5};
+    static int[] c = {5, 5};
+    static int[][] partOfSubsets = {{3, 3}, {1, 3}};
+    static int[][] partOfSubsets2 = {{2, 0}, {1, 2}, {1, 4}};
+    static int[][] partOfSubsets3 = {{2, 3}, {2, 3}};
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        MainController mv = new MainController();
+        //MainController mv = new MainController();
         // mv.showMainView();
         // mv.setActions();
+        buildtreeC45();
 
-        ArrayList<Double> entropies = new ArrayList<>();
-        String[] heather = {"temperatura", "ciśnienie", "wysokość", "samopoczucie"};
-        int[][] data = {
-            {1, 1, 0, 1},
-            {1, 1, 1, 1},
-            {1, 0, 0, 1},
-            {0, 0, 1, 1},
-            {0, 0, 1, 0},
-            {0, 0, 1, 0},
-            {0, 2, 1, 0},
-            {0, 2, 0, 0},
-            {1, 2, 0, 0},
-            {1, 2, 0, 0}};
-        double entropy;
-        int[] a = {6, 4};
-        int[] b = {2, 3, 5};
-        int[] c = {5, 5,};
-        int[][] partOfSubsets = {{3, 3}, {1, 3}};
-        int[][] partOfSubsets2 = {{2, 0}, {1, 2}, {1, 4}};
-        int[][] partOfSubsets3 = {{2, 3}, {2, 3}};
+    }
+
+    private static void buildtreeC45() {//ArrayList<DataObject> dObj, String... param) {
         C45 c45 = new C45();
-        System.out.println("etropia dec " + c45.calcEntropy(C45.ENTROPY.DECISION, 10, a, null));
-        entropy = c45.calcEntropy(C45.ENTROPY.ATTRIBUTE, 10, a, partOfSubsets);
-        System.out.println("etropia atr1 " + entropy);
-        entropies.add(entropy);
         entropy = c45.calcEntropy(C45.ENTROPY.ATTRIBUTE, 10, b, partOfSubsets2);
         System.out.println("etropia atr2 " + entropy);
         entropies.add(entropy);
-        entropy = c45.calcEntropy(C45.ENTROPY.ATTRIBUTE, 10, c, partOfSubsets3);
-        System.out.println("etropia atr3 " + entropy);
-        entropies.add(entropy);
-        System.out.println("zysk " + c45.calcGain(c45.calcEntropy(C45.ENTROPY.DECISION, 10, a, null), c45.calcEntropy(C45.ENTROPY.ATTRIBUTE, 10, a, partOfSubsets)));
-
     }
-
-    private void c45(ArrayList<DataObject> dObj, String... param) {
-
-    }
-
 }
