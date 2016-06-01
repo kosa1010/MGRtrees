1e39ac9eefe4254f379cfb7b95d6982a339fb5b2
kosa1010 <kosa1010@o2.pl>
2016-06-01 06:50:21 +0200

    Zliczanie liczby elementów

--- MGRTrees/src/main/java/com/mycompany/mgrtrees/C45.java
+++ MGRTrees/src/main/java/com/mycompany/mgrtrees/C45.java
@@ -6,6 +6,7 @@
 package com.mycompany.mgrtrees;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -21,6 +22,7 @@ public class C45 {
     }
 
     /**
+     * Metoda oblicza entropię
      *
      * @param en atrybut zwykły lub decyzyjny
      * @param allObj liczba wszkich objektów w tablicy
@@ -63,5 +65,51 @@ public class C45 {
      */
     public double calcGain(double entDec, double entAtrribute) {
         return entDec - entAtrribute;
+    }
+
+    /**
+     * Zlicza liczność podzbiorów na jakie są dzielone dane wedługo określonego
+     * atrybutu
+     *
+     * @param tab talbicza z danymi
+     * @param heather tablicza nazw nagłówków
+     * @return
+     */
+    public List<int[]> countDataSubsets(int tab[][], String[] heather) {
+        List<int[]> listOfSubsets = new ArrayList<>();
+        return listOfSubsets;
+    }
+
+    /**
+     * Zliczanie liczby elementów
+     *
+     * @param tab
+     * @return
+     */
+    private int[] countElements(int tab[]) {
+        ArrayList<Integer> countOfElems = new ArrayList<>();
+        boolean all = false;
+        int i = 0;
+        int sum;
+        while (!all) {
+            sum = 0;
+            for (int a : tab) {
+                if (tab[i] == a) {
+                    sum += 1;
+                }
+            }
+            countOfElems.add(sum);
+            i++;
+            int buf = 0;
+            buf = countOfElems.stream().map((s) -> s).reduce(buf, Integer::sum);//foreach sumujący el w liscie
+            if (buf >= tab.length) {
+                all = true;
+            }
+        }
+        int t[] = new int[countOfElems.size()];
+        for (int ii = 0; ii < countOfElems.size(); ii++) {
+            t[ii] = countOfElems.get(ii);
+        }
+        return t;
     }
 }
